968bc1944385e4655c2921f73525b5814c14a0d2
Seth Norton <snorton@osisoft.com>
2022-06-23 13:18:54 -0400

    Added test cases

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -10,5 +10,22 @@ public class MyTest {
 
     @Test
     public void test_method_2() {
+        assert true;
+    }
+    @Test
+    public void test_method_3() {
+        assert true;
+    }
+    @Test
+    public void test_method_4() {
+        assert true;
+    }
+    @Test
+    public void test_method_5() {
+        assert false;
+    }
+    @Test
+    public void test_method_6() {
+        assert false;
     }
 }
